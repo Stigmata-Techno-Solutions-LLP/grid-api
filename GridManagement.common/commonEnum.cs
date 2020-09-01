ae7b66e8aa976480e92e53819bbbe7fa2d70d725
amalnath <cap.poobalan@gmail.com>
2020-09-01 16:25:54 +0800

    swagger file generate with api property model

---
+++ GridManagement.common/commonEnum.cs
@@ -0,0 +1,23 @@
+namespace GridManagement.common
+{
+    public class commonEnum
+    {
+        public enum CG_RFIStatus {
+New ,
+InProgress,
+Completed
+
+        }
+        public enum LV_RFIStatus {
+New ,
+InProgress,
+Completed
+        }
+        public enum CT_RFIStatus{
+New ,
+InProgress,
+Completed
+        }
+    }
+
+}
